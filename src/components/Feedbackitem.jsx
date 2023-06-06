a7abc3a26a89a8233e012355dbc6a763a784577a
Aldren peshaia <aldrenpeshaia@hotmail.com>
2023-06-06 16:33:17 +0200

    Added alot of stuff in the files above, can be from deleteButton to RatingSelect and even changed alot in App.js where the context and the code is much smoother

--- src/components/Feedbackitem.jsx
+++ src/components/Feedbackitem.jsx
@@ -1,22 +1,25 @@
 import { FaTimes } from "react-icons/fa";
+import { useContext } from "react";
 import PropTypes from "prop-types";
+import FeedbackContext from "../context/FeedbackContext";
+
 import Card from "./shared/Card";
 
-function Feedbackitem({ item, handleDelete }) {
-  const handleClick = (id) => {
-    console.log(id);
-  };
+function Feedbackitem({ item }) {
+  const { deleteFeedback } = useContext(FeedbackContext);
   return (
     <Card>
       <div className="num-display">{item.rating}</div>
-      <button onClick={() => handleDelete(item.id)} className="close">
-        <FaTimes color="purple" />
+      <button className="close">
+        <FaTimes color="purple" onClick={() => deleteFeedback(item.id)} />
       </button>
       <div className="text-display">{item.text}</div>
     </Card>
   );
 }
+
 Feedbackitem.propTypes = {
   item: PropTypes.object.isRequired,
 };
+
 export default Feedbackitem;
