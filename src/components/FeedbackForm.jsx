5b94e5ee0081d256eb0064ee31da0529b889cb1f
Aldren peshaia <aldrenpeshaia@hotmail.com>
2023-06-07 00:25:17 +0200

    Added Spinner etc...

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -6,7 +6,8 @@ import FeedbackContext from "../context/FeedbackContext";
 
 function FeedbackForm() {
   const [text, setText] = useState("");
-  const [rating, setRating] = useState(10);
+  const [rating, setRating] = useState("10");
+
   const [btnDisabled, setBtnDisabled] = useState(true);
   const [message, setMessage] = useState("");
 
@@ -21,24 +22,20 @@ function FeedbackForm() {
     }
   }, [feedbackEdit]);
 
-  // NOTE: This should be checking input value not state as state won't be the updated value until the next render of the component
-
-  // prettier-ignore
-  const handleTextChange = ({ target: { value } }) => { // 👈  get the value
-    if (value === '') {
-      setBtnDisabled(true)
-      setMessage(null)
-      
-  // prettier-ignore
-    } else if (value.trim().length < 10) { // 👈 check for less than 10
-      setMessage('Text must be at least 10 characters')
-      setBtnDisabled(true)
+  const HandleTextChange = (e) => {
+    if (text === "") {
+      setBtnDisabled(true);
+      setMessage(null);
+    } else if (text !== "" && text.trim().length <= 10) {
+      setMessage("Text must be at least 10 characters");
+      setBtnDisabled(true);
     } else {
-      setMessage(null)
-      setBtnDisabled(false)
+      setMessage(null);
+      setBtnDisabled(false);
     }
-    setText(value)
-  }
+
+    setText(e.target.value);
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -47,29 +44,23 @@ function FeedbackForm() {
         text,
         rating,
       };
-
       if (feedbackEdit.edit === true) {
         updateFeedback(feedbackEdit.item.id, newFeedback);
       } else {
         addFeedback(newFeedback);
       }
-
-      // NOTE: reset to default state after submission
-      setBtnDisabled(true); // 👈  add this line to reset disabled
-      setRating(10); //👈 add this line to set rating back to 10
       setText("");
     }
   };
 
-  // NOTE: pass selected to RatingSelect so we don't need local duplicate state
   return (
     <Card>
       <form onSubmit={handleSubmit}>
-        <h2>How would you rate your service with us?</h2>
-        <RatingSelect select={setRating} selected={rating} />
+        <h2>How would you rate your service with us? </h2>
+        <RatingSelect select={(rating) => setRating(rating)} />
         <div className="input-group">
           <input
-            onChange={handleTextChange}
+            onChange={HandleTextChange}
             type="text"
             placeholder="Write a review"
             value={text}
@@ -78,7 +69,6 @@ function FeedbackForm() {
             Send
           </Button>
         </div>
-
         {message && <div className="message">{message}</div>}
       </form>
     </Card>
